019ed5f47453cc553fa6555a52a08da16f0644af
Vfivew <hitori555111555@yahoo.com>
2024-04-08 19:18:54 +0300

    fix(frontend): hotfix

--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,10 @@ import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
 
 import productsReducer from "../reducers/productsReducer";
-import seedProductReducer from "../reducers/seedProductReducer";
 import basketReducer from "../reducers/basketReducer";
 import paginationReducer from "../reducers/paginationReducer";
 import formReducer from "../reducers/formReducer";
+import seedProductReducer from "../reducers/seedProductReducer";
 
 const rootReducer = combineReducers({
   products: productsReducer,
